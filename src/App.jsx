93477f09ac027f08360470202621af69ac306e07
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-09-04 23:10:44 +0100

    code tidy up

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ import GameOver from "./components/GameOver";
 
 import { WINNING_COMBINATIONS } from './winning-combinations';
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -23,16 +28,8 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  
-  const [ players, setPlayers ] = useState({
-    'X': 'Player 1',
-    'O': 'Player 2'
-  });
-  const [ gameTurns, setGameTurns ] = useState([]);
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard].map(array => [...array]);
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD].map(array => [...array]);
 
   for(const turn of gameTurns) {
     const { square, player } = turn;
@@ -40,7 +37,10 @@ function App() {
 
     gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
 
+function deriveWinner(gameBoard, players) {
   let winner = null;
 
   // check for winner
@@ -55,6 +55,18 @@ function App() {
     }
   }
 
+  return winner;
+}
+
+function App() {
+  
+  const [ players, setPlayers ] = useState(PLAYERS);
+  const [ gameTurns, setGameTurns ] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
